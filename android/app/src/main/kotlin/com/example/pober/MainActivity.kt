089c049e66a1ddb4f4938264863cae1598f67d8c
Khachenchai <jaiklaanda@gmail.com>
2022-04-27 21:32:40 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/pober/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pober
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/view/authen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AuthenPage extends StatefulWidget {
+  const AuthenPage({ Key? key }) : super(key: key);
+
+  @override
+  State<AuthenPage> createState() => _AuthenPageState();
+}
+
+class _AuthenPageState extends State<AuthenPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
